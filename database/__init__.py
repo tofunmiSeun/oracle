b124a9a60c996ba511c5f9cdaba2eceea603bbe7
Tofunmi Ogungbaigbe <tofunmiseun@gmail.com>
2024-01-26 22:09:28 +0100

    code refactoring

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,4 @@
 import pymongo
-from pymongo.database import Database
 import os
 from dotenv import load_dotenv
 from database.service import DatabaseService
@@ -9,11 +8,10 @@ load_dotenv()
 uri = os.getenv('MONGODB_URI')
 # Todo: fix TLS certificate validation
 mongo_client = pymongo.MongoClient(uri, tlsAllowInvalidCertificates=True)
-mongo_database: Database = mongo_client.oracle
 
 try:
     mongo_client.admin.command('ping')
 except Exception as e:
     print(e)
 
-db_service = DatabaseService(mongo_database)
+db_service = DatabaseService(mongo_client.oracle)
