d398aff1cba2a952543437b53307272dbf877b18
Tofunmi Ogungbaigbe <tofunmiseun@gmail.com>
2024-01-31 20:25:27 +0100

    updating model requirements

--- api/models.py
+++ api/models.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CreateNamespaceRequest(BaseModel):
-    title: str
-    description: str
+    title: str = Field(min_length=3)
+    description: Optional[str]
 
 
 class CreateDatasourceRequest(BaseModel):
