780eef07b68275397ff2304a57ddd142460f3067
Tofunmi Ogungbaigbe <tofunmiseun@gmail.com>
2024-02-14 11:20:39 +0100

    highlighting required env variables vs optional ones

--- loaded_env_variables.py
+++ loaded_env_variables.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Required
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 MONGODB_URI = os.getenv('MONGODB_URI')
+
+# Optional
 FRONTEND_URL = os.getenv('FRONTEND_URL')
